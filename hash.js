3212b8ee0df004709664989c509cbbbd13606798
RamGamezly <45399638+RamGamezly@users.noreply.github.com>
2019-10-07 11:18:23 -0400

    Add files via upload

---
+++ hash.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  if (window.location.href.indexOf('#Setup') != -1) {
+    $('#Setup').modal('show');
+  }
+
+});
+
+$(document).ready(function() {
+
+  if (window.location.href.indexOf('#Commands') != -1) {
+    $('#Commands').modal('show');
+  }
+
+});
+
+$(document).ready(function() {
+
+  if (window.location.href.indexOf('#Help') != -1) {
+    $('#Help').modal('show');
+  }
+
+});
+
+$(document).ready(function() {
+
+  if (window.location.href.indexOf('#Zamasu') != -1) {
+    $('#Zamasu').modal('show');
+  }
+
+});
